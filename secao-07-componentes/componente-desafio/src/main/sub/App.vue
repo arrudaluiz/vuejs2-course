66c07c040b9335b3455fbeb3a8e94e636d02a954
Marcos <marcos.okamura@guenka.com.br>
2020-01-17 11:20:21 -0300

    Finalização da seção sobre componentes

---
+++ secao-07-componentes/componente-desafio/src/main/sub/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <MenuBar />
+
+    <TitleBar />
+
+    <v-content>
+      <Carousel />
+    </v-content>
+
+    <PageFooter />
+  </v-app>
+</template>
+
+<script>
+/** Caminhos relativos */
+import MenuBar from "../../components/MenuBar.vue";
+import TitleBar from "../../components/TitleBar.vue";
+/** Caminhos absolutos */
+import Carousel from "@/widgets/Carousel.vue";
+import PageFooter from "@/components/PageFooter.vue";
+
+export default {
+  components: { MenuBar, TitleBar, Carousel, PageFooter }
+};
+</script>
