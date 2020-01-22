76c7b958b210647a173247c445b17cfc5ab91054
Marcos <marcos.okamura@guenka.com.br>
2020-01-22 14:40:25 -0300

    Atualização da seção 10, projeto 2

---
+++ secao-10-projeto-02-tarefas/tarefas/src/components/NewTask.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="new-task">
+    <input type="text" :placeholder="value" />
+    <button>
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      required: false
+    }
+  }
+};
+</script>
+
+<style scoped>
+.new-task {
+  display: flex;
+  justify-content: center;
+  width: 30%;
+  height: 30pt;
+  margin-bottom: 30pt;
+}
+input {
+  color: white;
+  background-color: rgba(255, 255, 255, 0.1);
+  width: 100%;
+  height: 100% - 2px;
+  padding: 8px;
+  border-radius: 5pt 0 0 5pt;
+  border: 1px solid grey;
+  border-right: none;
+}
+button {
+  color: white;
+  font-weight: 700;
+  background-color: #2594f0;
+  height: 100%;
+  border-radius: 0 5pt 5pt 0;
+  border: 1px solid grey;
+  border-left: none;
+}
+</style>
---
+++ secao-10-projeto-02-tarefas/tarefas/src/components/ProgressBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="progress-bar">
+    <div class="progress" :style="{width: progress + '%'}"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    progress: {
+      type: Number,
+      default: 0
+    }
+  }
+};
+</script>
+
+<style scoped>
+.progress-bar {
+  width: 80%;
+  height: 15pt;
+  border-radius: 5pt;
+  border: 1px solid grey;
+  margin-bottom: 30pt;
+}
+
+.progress {
+  height: 100%;
+  border-radius: 4pt;
+  background-color: #49b057;
+}
+</style>
