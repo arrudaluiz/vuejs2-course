6460c5aa044bdd73b4114c6568f5ce520b8bb569
Marcos <marcos.okamura@guenka.com.br>
2020-01-28 14:15:10 -0300

    Atualização da seção 12, tópico 180

--- secao-12-diretivas/diretivas-exercicios/src/App.vue
+++ secao-12-diretivas/diretivas-exercicios/src/App.vue
@@ -1,23 +1,35 @@
 <template>
-	<div id="app">
-		<h1>Diretivas</h1>
-	</div>
+  <div id="app">
+    <h1>Diretivas</h1>
+    <hr />
+    <p v-text="text"></p>
+    <p v-html="html"></p>
+    <hr />
+    <p v-highlight>Usando diretiva personalizada</p>
+  </div>
 </template>
 
 <script>
-export default {
-	
-}
+  export default {
+    data() {
+      return {
+        text:
+          'Usando diretiva v-text. <strong>Tags HTML não são interpretadas</strong>',
+        html:
+          'Usando diretiva v-html <strong>(Cuidado com ataques XSS: tenha certeza a fonte da informação é segura.)</strong>'
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-	font-size: 2.5rem;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    font-size: 2.5rem;
+  }
 </style>
--- secao-12-diretivas/diretivas-exercicios/src/main.js
+++ secao-12-diretivas/diretivas-exercicios/src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.directive('highlight', {
+  bind(el) {
+    el.style.backgroundColor = 'lightgreen'
+  }
+})
 new Vue({
-	render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
