6270811074a7ba1a018eeb641ba8db17ef20e20c
Marcos <marcos.okamura@guenka.com.br>
2020-01-29 09:26:03 -0300

    Alteração do tópico 185

--- secao-12-diretivas/diretivas-exercicios/src/App.vue
+++ secao-12-diretivas/diretivas-exercicios/src/App.vue
@@ -9,11 +9,34 @@
       Usando diretiva personalizada
     </p>
     <p v-highlight.delay="color">Usando diretiva personalizada</p>
+    <hr />
+    <p v-local-highlight:background.delay="'lightyellow'">
+      Usando diretiva personalizada local
+    </p>
+    <p v-local-highlight.delay="color">Usando diretiva personalizada local</p>
   </div>
 </template>
 
 <script>
   export default {
+    directives: {
+      'local-highlight': {
+        bind(el, binding) {
+          // el.style.backgroundColor = 'lightgreen'
+
+          let delay = 0
+          if (binding.modifiers['delay']) delay = 2000
+
+          setTimeout(() => {
+            if (binding.arg === 'background') {
+              el.style.backgroundColor = binding.value
+            } else {
+              el.style.color = binding.value
+            }
+          }, delay)
+        }
+      }
+    },
     data() {
       return {
         text:
