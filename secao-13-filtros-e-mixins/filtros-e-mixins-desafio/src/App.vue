1d35b5420ac8e134c75d938c41562cd5632ab8ca
Marcos <marcos.okamura@guenka.com.br>
2020-01-31 13:26:53 -0300

    Atualização da seção 13, tópico 199

--- secao-13-filtros-e-mixins/filtros-e-mixins-desafio/src/App.vue
+++ secao-13-filtros-e-mixins/filtros-e-mixins-desafio/src/App.vue
@@ -1,36 +1,65 @@
 <template>
-	<div id="app">
-		<h1>Filtros & Mixins (Desafio)</h1>
-		<!-- Exercício 1 -->
-		<!-- Construir um filtro local que troca espaços por vírgula -->
-		
-		<!-- Exercício 2 -->
-		<!-- Filtro global que conta o tamanho de cada palavra e adiciona o 
+  <div id="app">
+    <h1>Filtros & Mixins (Desafio)</h1>
+    <!-- Exercício 1 -->
+    <!-- Construir um filtro local que troca espaços por vírgula -->
+    <p>{{ text1 | spaceToComma }}</p>
+    <p>{{ text1 | space-to-comma }}</p>
+    <!-- Exercício 2 -->
+    <!-- Filtro global que conta o tamanho de cada palavra e adiciona o 
 			valor na string final -->
-		<!-- "Pedro é legal" => "Pedro (5) é (1) legal (5)" -->
+    <!-- "Pedro é legal" => "Pedro (5) é (1) legal (5)" -->
+    <p>{{ text2 }} => {{ text2 | wordLength }}</p>
 
-		<!-- Exercício 3 -->
-		<!-- Implementar os exercicios 1 e 2 com propriedade computada -->
+    <!-- Exercício 3 -->
+    <!-- Implementar os exercicios 1 e 2 com propriedade computada -->
+    <p>{{ spaceToCommaComputed }}</p>
+    <p>{{ text2 }} => {{ wordLengthComputed }}</p>
+    <input type="text" v-model="text1" />
+    <input type="text" v-model="text2" />
 
-		<!-- Exercício 4 -->
-		<!-- Compartilhe a propriedade computada via mixin -->
-	</div>
+    <!-- Exercício 4 -->
+    <!-- Compartilhe a propriedade computada via mixin -->
+  </div>
 </template>
 
 <script>
-export default {
-	
-}
+  export default {
+    filters: {
+      spaceToComma(value) {
+        // return value.split(' ').join(',')
+        return value.replace(/\s/g, ',')
+      }
+    },
+    data() {
+      return {
+        text1: 'Testando filtro local que troca espaços por vírgula',
+        text2: 'Pedro é legal'
+      }
+    },
+    computed: {
+      spaceToCommaComputed() {
+        return this.text1.split(' ').join(',')
+      },
+      wordLengthComputed() {
+        const arr = this.text2.split(' ')
+        arr.forEach((element, i) => {
+          arr[i] = element.concat(' (' + element.length + ')')
+        })
+        return arr.join(' ')
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-	font-size: 2.5rem;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    font-size: 2.5rem;
+  }
 </style>
--- secao-13-filtros-e-mixins/filtros-e-mixins-desafio/src/main.js
+++ secao-13-filtros-e-mixins/filtros-e-mixins-desafio/src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.filter('wordLength', value => {
+  const arr = value.split(' ')
+  arr.forEach((element, i) => {
+    arr[i] = element.concat(' (' + element.length + ')')
+  })
+  return arr.join(' ')
+
+  // return value.split(' ').map(w => { `${w} (${w.length})`}).join(' ')
+})
+
 new Vue({
-	render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
