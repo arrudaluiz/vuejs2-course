9b770a6c5270c44e2d59372af9746e6325ad6785
Luiz Arruda <arrudaluiz89@gmail.com>
2020-02-01 22:16:35 -0300

    Alteração do tópico 205

--- secao-14-animacoes/animacoes-exercicios/src/App.vue
+++ secao-14-animacoes/animacoes-exercicios/src/App.vue
@@ -1,24 +1,35 @@
 <template>
-	<div id="app" class="container-fluid">
-		<h1>Animações</h1>
-	</div>
+  <div id="app" class="container-fluid">
+    <h1>Animações</h1>
+    <hr />
+    <b-button variant="primary" class="mb-4"
+			@click="visible = !visible">Mostrar Mensagem</b-button>
+    
+		<transition>
+      <b-alert variant="info" show v-if="visible">{{ message }}</b-alert>
+    </transition>
+  </div>
 </template>
 
 <script>
-
-export default {
-
-}
+  export default {
+    data() {
+      return {
+        message: "Mensagem de informação",
+        visible: false
+      };
+    }
+  };
 </script>
 
 <style>
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-	font-size: 1.5rem;
-}
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    font-size: 1.5rem;
+  }
 </style>
