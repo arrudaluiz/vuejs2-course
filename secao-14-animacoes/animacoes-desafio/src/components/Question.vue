dc503f30d4739c424d1b671855e92ce5fa630a77
Marcos <marcos.okamura@guenka.com.br>
2020-02-04 13:20:41 -0300

    Alteração do tópico 224

--- secao-14-animacoes/animacoes-desafio/src/components/Question.vue
+++ secao-14-animacoes/animacoes-desafio/src/components/Question.vue
@@ -1,57 +1,61 @@
 <template>
-    <div class="question">
-        
-    </div>
+  <div class="question">
+    <h2>Pergunta teste</h2>
+    <ul class="answers">
+      <li><div class="number">1</div><p class="text">Teste 1</p></li>
+      <li><div class="number">2</div><p class="text">Teste 2</p></li>
+      <li><div class="number">3</div><p class="text">Teste 3</p></li>
+      <li><div class="number">4</div><p class="text">Teste 4</p></li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+  export default {}
 </script>
 
 <style>
-    .question {
-        height: 400px;
-        color: #000;
-        background-color: #FFF;
-        width: 70%;
-        border-radius: 20px;
-        font-size: 2.5rem;
+  .question {
+    height: 400px;
+    color: #000;
+    background-color: #fff;
+    width: 70%;
+    border-radius: 20px;
+    font-size: 2.5rem;
 
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-    }
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
 
-    ul.answers {
-        font-size: 2rem;
-        padding: 0;
-        display: flex;
-        justify-content: space-around;
-        flex-wrap: wrap;
-    }
+  ul.answers {
+    font-size: 2rem;
+    padding: 0;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+  }
 
-    .answers li {
-        margin: 20px;
-        background-color: #89c454;
-        border-radius: 8px;
-        width: 40%;
+  .answers li {
+    margin: 20px;
+    background-color: #89c454;
+    border-radius: 8px;
+    width: 40%;
 
-        display: flex;
-    }
+    display: flex;
+  }
 
-    .answers .number {
-        padding: 10px;
-        background-color: #1e9c31;
-        color: #FFF;
-        flex-basis: 30px;
-        border-top-left-radius: 8px;
-        border-bottom-left-radius: 8px;
-    }
+  .answers .number {
+    padding: 10px;
+    background-color: #1e9c31;
+    color: #fff;
+    flex-basis: 30px;
+    border-top-left-radius: 8px;
+    border-bottom-left-radius: 8px;
+  }
 
-    .answers .text {
-        flex: 1;
-        align-self: center;
-    }
+  .answers .text {
+    flex: 1;
+    align-self: center;
+  }
 </style>
