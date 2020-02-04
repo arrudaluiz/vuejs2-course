88748c69fe003c56ec4c99713b8af7943ff9a08e
Marcos <marcos.okamura@guenka.com.br>
2020-02-04 09:40:24 -0300

    Alteração do tópico 220

--- secao-14-animacoes/animacoes-exercicios/src/App.vue
+++ secao-14-animacoes/animacoes-exercicios/src/App.vue
@@ -2,10 +2,11 @@
   <div id="app" class="container-fluid">
     <h1>Animações</h1>
     <hr />
-    <b-button variant="primary" class="mb-4"
-			@click="visible = !visible">Alternar Mensagem</b-button>
-    
-		<!-- <transition name="fade" appear>
+    <b-button variant="primary" class="mb-4" @click="visible = !visible"
+      >Alternar Mensagem</b-button
+    >
+
+    <!-- <transition name="fade" appear>
       <b-alert variant="info" show v-if="visible">{{ message }}</b-alert>
     </transition>
 
@@ -14,7 +15,7 @@
     </transition> -->
 
     <!-- https://daneden.github.io/animate.css/ -->
-		<!-- <transition
+    <!-- <transition
       enter-active-class="animated fadeInLeft"
       leave-active-class="animated fadeOutRight">
       <b-alert variant="info" show v-show="visible">{{ message }}</b-alert>
@@ -25,72 +26,112 @@
       <option value="slide">Slide</option>
     </b-select>
 
-		<transition :name="animationType" mode="out-in">
-      <b-alert variant="info" show v-if="visible" key="info">{{ message }}</b-alert>
+    <transition :name="animationType" mode="out-in">
+      <b-alert variant="info" show v-if="visible" key="info">{{
+        message
+      }}</b-alert>
       <b-alert variant="warning" show v-else key="warn">{{ message }}</b-alert>
     </transition>
 
-    <hr>
-    <button @click="visible2 = !visible2">{{ visible2 ? 'Ocultar' : 'Exibir' }}</button>
+    <hr />
+    <button @click="visible2 = !visible2">
+      {{ visible2 ? 'Ocultar' : 'Exibir' }}
+    </button>
     <transition
       :css="false"
       @before-enter="beforeEnter"
       @enter="enter"
-
+      @after-enter="afterEnter"
+      @enter-cancelled="enterCancelled"
       @before-leave="beforeLeave"
-      @leave="leave">
+      @leave="leave"
+      @after-leave="afterLeave"
+      @leave-cancelled="leaveCancelled"
+      appear
+    >
       <div v-if="visible2" class="box"></div>
+    </transition>
+
+    <hr />
+    <div class="mb-4">
+      <b-button variant="primary" class="mr-2"
+        @click="selectedComponent = 'InfoAlert'">Informação</b-button>
+      <b-button variant="secondary"
+        @click="selectedComponent = 'WarningAlert'">Advertência</b-button>
+    </div>
+
+    <transition name="fade" mode="out-in">
+      <component :is="selectedComponent"></component>
     </transition>
   </div>
 </template>
 
 <script>
+  import WarningAlert from './WarningAlert'
+  import InfoAlert from './InfoAlert'
+
   export default {
+    components: { WarningAlert, InfoAlert },
     data() {
       return {
-        message: "Mensagem de informação",
+        message: 'Mensagem de informação',
         visible: false,
         visible2: true,
-        animationType: "fade",
-        baseWidth: 300
-      };
+        animationType: 'fade',
+        baseWidth: 300,
+        selectedComponent: 'InfoAlert'
+      }
     },
     methods: {
-      animate(el, done, width, duration, show) {
-        const interval = 16
-        const totalRound = duration / interval
-        const widthIncrement = this.baseWidth / totalRound
+      animate(el, done, show) {
+        const interval = 20
+        const roundTotal = 50
+        const widthIncrement = 6
 
         let round = 1
         const timer = setInterval(() => {
-          const newWidth = width + (round * show) * widthIncrement
+          const newWidth = show
+            ? round * widthIncrement
+            : this.baseWidth - round * widthIncrement
           el.style.width = `${newWidth}px`
           round++
-          if (round > totalRound) {
+          if (round > roundTotal) {
             clearInterval(timer)
             done()
           }
-        }, interval);
+        }, interval)
       },
       beforeEnter(el) {
         el.style.width = 0
       },
       enter(el, done) {
-        this.animate(el, done, el.clientWidth, 1000, true)
+        this.animate(el, done, true)
       },
+      // afterEnter(el) {
+      //   console.log('afterEnter');
+      // },
+      // enterCancelled(el) {
+      //   console.log('enterCancelled');
+      // },
       beforeLeave(el) {
         el.style.width = `${this.baseWidth}px`
       },
       leave(el, done) {
-        this.animate(el, done, el.clientWidth, 1000, false)
+        this.animate(el, done, false)
       }
+      // afterLeave(el) {
+      //   console.log('afterLeave');
+      // },
+      // leaveCancelled(el) {
+      //   console.log('leaveCancelled');
+      // }
     }
-  };
+  }
 </script>
 
 <style>
   #app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
